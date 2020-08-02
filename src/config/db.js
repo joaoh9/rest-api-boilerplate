a2a26153b1e47f471ba17005dbb634d05a082ada
João Henrique <joaoh9.costa@gmail.com>
2020-08-02 01:04:37 -0300

    delete unused files

--- src/config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  dbServer: process.env.DB_SERVER,
-  dbName: process.env.DB_NAME,
-  port: parseInt(process.env.PORT)
-};
--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-const db = require('./db')
-const Logger = require('./logger');
-
-module.exports = {
-  db,
-  Logger
-};
